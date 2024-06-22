767fd932b32c0731812570174dcb4ad387a31aea
Divyansh8x <Divyansh8x@users.noreply.github.com>
2024-06-23 00:06:44 +0530

    Add Lazy Loading!

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,24 +1,51 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-import Home from './Components/Home'
-import SocialLinks from './Components/SocialLinks'
-import About from './Components/About'
-import Projects from './Components/Projects'
-import TechStack from './Components/TechStack'
-import Contact from './Components/Contact'
+import { lazy, Suspense } from "react";
+import Lottie from "react-lottie";
+import animationData from "./assets/Loading.json";
+
+const Navbar = lazy(() => import("./Components/Navbar"));
+const Home = lazy(() => import("./Components/Home"));
+const SocialLinks = lazy(() => import("./Components/SocialLinks"));
+const About = lazy(() => import("./Components/About"));
+const Projects = lazy(() => import("./Components/Projects"));
+const TechStack = lazy(() => import("./Components/TechStack"));
+const Contact = lazy(() => import("./Components/Contact"));
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
 const App = () => {
   return (
-    <div>
-     <Navbar/>
-     <Home/>
-     <About/>
-     <Projects/>
-     <TechStack/>
-     <Contact/>
+    <Suspense
+      fallback={
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <Lottie options={defaultOptions} height={200} width={200} />
+        </div>
+      }
+    >
+      <div>
+        <Navbar />
+        <Home />
+        <About />
+        <Projects />
+        <TechStack />
+        <Contact />
+        <SocialLinks />
+      </div>
+    </Suspense>
+  );
+};
 
-     <SocialLinks/>
-    </div>
-  )
-}
-
-export default App
+export default App;
